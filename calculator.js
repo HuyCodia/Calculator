0933b1ebc90277af769dc6543a0fa2046ff30d11
Huy Dang <huyd903@gmail.com>
2022-04-02 20:26:19 +0200

    add global eventListener for container

--- calculator.js
+++ calculator.js
@@ -1,22 +1,23 @@
+let container;
+
+window.addEventListener("load", function() {
+  container = document.getElementById('resultArea');
+}, {once: true});
+
 function appendOperation(operation) {
-    let container = document.getElementById('resultArea');
     if (container.innerHTML == 0) {
         document.getElementById('resultArea').innerHTML = operation;
     } else {
         document.getElementById('resultArea').innerHTML += operation;
     }
-
 }
 
 function calculateResult() {
-    let container = document.getElementById('resultArea');
     let result = eval(container.innerHTML);  
     container.innerHTML = result;
 }
 
 function deleteLast() {
-    let container = document.getElementById('resultArea');
-
     if (container.innerHTML.endsWith(' ')) {
         container.innerHTML = container.innerHTML.slice(0, -3);
         if (container.innerHTML.length == 0) {
@@ -31,6 +32,5 @@ function deleteLast() {
 }
 
 function resetInput() {
-    let container = document.getElementById('resultArea');
     container.innerHTML = 0;
 }
